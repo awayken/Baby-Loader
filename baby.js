982f94f88c42dc51627a6f743bc5fe631a8a3b95
Miles Rausch <miles@milesrausch.com>
2012-03-16 22:56:18 -0500

    Fixed decimal points and min-width for styling.
    Fixed the decimal points on the percent loaded to 8 points. Set minimum
    width on percent loaded to 96px for better styling.

--- baby.js
+++ baby.js
@@ -30,14 +30,14 @@
 		};
 		
 		this.update = function () {
-			var that = this, itemid = 'babyprogress_' + Date.parse(this.duedate), item = window.document.getElementById(itemid), percentdone = ((1 - (this.duedate - new Date()) / this.gestation) * 100), s = '', h = '', done = '';
+			var that = this, itemid = 'babyprogress_' + Date.parse(this.duedate), item = window.document.getElementById(itemid), percentdone = ((1 - (this.duedate - new Date()) / this.gestation) * 100).toFixed(8), s = '', h = '', done = '';
 			
 			if (percentdone >= 100) {
 				percentdone = 100;
 			}
 			
 			if (!item) {
-				s = '<style>#' + itemid + '{border:1px solid #000;background:#eee;overflow:hidden;-webkit-border-radius:12px;-moz-border-radius:12px;border-radius:12px;-moz-background-clip:padding;-webkit-background-clip:padding-box;background-clip:padding-box;} #' + itemid + '>div{background-color:' + this.color.from + ';background-image:-webkit-gradient(linear,left top,left bottom,from(' + this.color.from + '),to(' + this.color.to + '));background-image:-webkit-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-moz-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-ms-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-o-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:linear-gradient(to bottom,' + this.color.from + ',' + this.color.to + ');;width:' + percentdone + '%;min-width:17%;color:#fff;text-align:right;padding-right:6px;-webkit-border-radius:12px;-moz-border-radius:12px;border-radius:12px;}</style>';
+				s = '<style>#' + itemid + '{border:1px solid #000;background:#eee;overflow:hidden;-webkit-border-radius:12px;-moz-border-radius:12px;border-radius:12px;-moz-background-clip:padding;-webkit-background-clip:padding-box;background-clip:padding-box;} #' + itemid + '>div{background-color:' + this.color.from + ';background-image:-webkit-gradient(linear,left top,left bottom,from(' + this.color.from + '),to(' + this.color.to + '));background-image:-webkit-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-moz-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-ms-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:-o-linear-gradient(top,' + this.color.from + ',' + this.color.to + ');background-image:linear-gradient(to bottom,' + this.color.from + ',' + this.color.to + ');width:' + percentdone + '%;min-width:96px;color:#fff;text-align:right;padding-right:6px;-webkit-border-radius:12px;-moz-border-radius:12px;border-radius:12px;}</style>';
 				window.document.write(s);
 				
 				h = '<div id="' + itemid + '" class="babyprogress"><div>' + percentdone + '%</div></div>';
